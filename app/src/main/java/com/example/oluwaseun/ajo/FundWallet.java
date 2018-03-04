6aabaf1065548929dafbfe0ab8e6eadff9cafd04
= <=>
2018-03-04 01:58:18 +0100

    Fund and Withdrwal working

---
+++ app/src/main/java/com/example/oluwaseun/ajo/FundWallet.java
@@ -0,0 +1,13 @@
+package com.example.oluwaseun.ajo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FundWallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fund_wallet);
+    }
+}
